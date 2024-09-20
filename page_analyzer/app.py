65086e1ebf3c82d3d83ac4e055fcb1ab7c4d546c
ithemask <ai77xxxx@gmail.com>
2024-09-20 18:07:34 +0300

    Update app.py module
    Add index() view function; add idle view functions required for template rendering

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,9 +2,25 @@ from os import getenv
 from dotenv import load_dotenv
 from flask import (
     Flask,
+    render_template,
 )
 
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.get('/urls')
+def get_urls():
+    pass
+
+
+@app.post('/urls')
+def post_url():
+    pass
