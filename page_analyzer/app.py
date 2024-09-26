c92ee3efad5a430b4f8fb4e334de2a6eb10b9a1c
ithemask <ai77xxxx@gmail.com>
2024-09-26 14:23:59 +0300

    Update app.py module
    Implement get_urls(), post_url() view functions; add show_url() view function

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,16 @@
 import psycopg2
 from os import getenv
 from dotenv import load_dotenv
+from page_analyzer.normalizer import normalize
+from page_analyzer.validator import is_valid
+from page_analyzer.db_access import UrlData
 from flask import (
     Flask,
     render_template,
+    request,
+    redirect,
+    url_for,
+    flash,
 )
 
 
@@ -12,18 +19,34 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 DATABASE_URL = getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
+url_data = UrlData(conn)
 
 
-@app.route('/')
+@app.get('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name='')
 
 
 @app.get('/urls')
 def get_urls():
-    pass
+    objects = url_data.get_content()
+    return render_template('urls/index.html', objects=objects)
 
 
 @app.post('/urls')
 def post_url():
-    pass
+    name = request.form.get('url')
+    if is_valid(name):
+        id = url_data.save(normalize(name))
+        flash('Страница успешно добавлена')
+        return redirect(url_for('show_url', id=id), 302)
+    flash('Некорректный URL')
+    return render_template('index.html', name=name), 422
+
+
+@app.get('/urls/<id>')
+def show_url(id):
+    object = url_data.find(id)
+    if object:
+        return render_template('urls/show.html', object=object)
+    return render_template('not_found.html'), 404
