1afb8c99ef693c48358c65b5b831a07ae526bcfc
ithemask <ai77xxxx@gmail.com>
2024-10-01 13:44:57 +0300

    Update db_access.py module
    Add get_checks(), save_check() methods

--- page_analyzer/db_access.py
+++ page_analyzer/db_access.py
@@ -66,3 +66,44 @@ class UrlData:
                 id = cur.fetchone()[0]
         conn.close()
         return id
+
+    def get_checks(self, id):
+        sql = '''
+            SELECT * FROM url_checks
+            WHERE url_id = %s
+            ORDER BY created_at DESC;
+        '''
+        conn = self.__get_db_conn()
+        with conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute(sql, (id,))
+                entries = cur.fetchall()
+        conn.close()
+        return entries
+
+    def save_check(self, check_result):
+        sql = '''
+            INSERT INTO url_checks (
+                url_id,
+                status_code,
+                h1,
+                title,
+                description,
+                created_at
+            )
+            VALUES (
+                %(url_id)s,
+                %(status_code)s,
+                %(h1)s,
+                %(title)s,
+                %(description)s,
+                %(created_at)s
+            );
+        '''
+        check_result['created_at'] = datetime.now()
+        conn = self.__get_db_conn()
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, check_result)
+        conn.close()
+        return
