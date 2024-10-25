81c254a067c8728edf8e19012fd6e0a163d12226
ithemask <ai77xxxx@gmail.com>
2024-10-26 00:40:13 +0300

    Update normalizer.py module
    Simplify normalize() function

--- page_analyzer/normalizer.py
+++ page_analyzer/normalizer.py
@@ -3,9 +3,4 @@ from urllib.parse import urlparse
 
 def normalize(url):
     components = urlparse(url)
-    return components._replace(
-        path='',
-        params='',
-        query='',
-        fragment='',
-    ).geturl()
+    return f'{components.scheme}://{components.netloc}'
