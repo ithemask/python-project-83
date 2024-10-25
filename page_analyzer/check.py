ba256d8d6029204e2b310f6d76e7acddf3badbb3
ithemask <ai77xxxx@gmail.com>
2024-10-26 00:12:12 +0300

    Refactor check.py module
    Add get_response_data() function; isolate HTTP request process

--- page_analyzer/check.py
+++ page_analyzer/check.py
@@ -2,19 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def check(url):
+def get_response_data(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
     except requests.RequestException:
         return None
+    return {'status': response.status_code, 'body': response.text}
+
+
+def check(url):
+    response_data = get_response_data(url)
+    if not response_data:
+        return None
     result = {
-        'status_code': response.status_code,
+        'status_code': response_data['status'],
         'h1': None,
         'title': None,
         'description': None,
     }
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response_data['body'], 'html.parser')
     h1 = soup.h1
     if h1:
         result['h1'] = h1.string
