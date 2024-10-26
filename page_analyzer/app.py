9153472b91c34019a3bc87a424af7538a4be2860
ithemask <ai77xxxx@gmail.com>
2024-10-26 13:56:36 +0300

    Update db_access.py, app.py modules
    Remove URL data class; move database connection management to controller layer

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,8 @@
 from os import getenv
 from dotenv import load_dotenv
+from page_analyzer import db_access
 from page_analyzer.normalizer import normalize
 from page_analyzer.validator import is_valid
-from page_analyzer.db_access import UrlData
 from page_analyzer.check import check
 from flask import (
     Flask,
@@ -19,7 +19,6 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = getenv('DATABASE_URL')
-url_data = UrlData(app.config['DATABASE_URL'])
 
 
 @app.get('/')
@@ -29,46 +28,68 @@ def index():
 
 @app.get('/urls')
 def get_urls():
-    url_info = url_data.get_url_info()
-    return render_template('urls/index.html', url_info=url_info)
+    conn = db_access.get_db_conn(app.config['DATABASE_URL'])
+    with conn:
+        url_info = db_access.get_url_info(conn)
+        response = render_template('urls/index.html', url_info=url_info)
+    conn.close()
+    return response
 
 
 @app.post('/urls')
 def post_url():
-    url_name = request.form.get('url')
-    if not is_valid(url_name):
-        flash('Некорректный URL', 'danger')
-        return render_template('index.html', url_name=url_name), 422
-    url_name = normalize(url_name)
-    url_id = url_data.get_url_id(url_name)
-    if url_id:
-        flash('Страница уже существует', 'info')
-    else:
-        url_id = url_data.save_url(url_name)
-        flash('Страница успешно добавлена', 'success')
-    return redirect(url_for('show_url', url_id=url_id), 302)
+    conn = db_access.get_db_conn(app.config['DATABASE_URL'])
+    with conn:
+        url_name = request.form.get('url')
+        if not is_valid(url_name):
+            flash('Некорректный URL', 'danger')
+            response = render_template('index.html', url_name=url_name), 422
+        else:
+            url_name = normalize(url_name)
+            url_id = db_access.get_url_id(conn, url_name)
+            if url_id:
+                flash('Страница уже существует', 'info')
+            else:
+                url_id = db_access.save_url(conn, url_name)
+                flash('Страница успешно добавлена', 'success')
+            response = redirect(url_for('show_url', url_id=url_id), 302)
+    conn.close()
+    return response
 
 
 @app.get('/urls/<url_id>')
 def show_url(url_id):
-    url = url_data.find_url(url_id)
-    if url:
-        checks = url_data.get_checks(url_id)
-        return render_template('urls/show.html', url=url, checks=checks)
-    abort(404)
+    conn = db_access.get_db_conn(app.config['DATABASE_URL'])
+    with conn:
+        url = db_access.find_url(conn, url_id)
+        if url:
+            checks = db_access.get_checks(conn, url_id)
+            response = render_template(
+                'urls/show.html',
+                url=url,
+                checks=checks,
+            )
+        else:
+            response = abort(404)
+    conn.close()
+    return response
 
 
 @app.post('/urls/<url_id>/checks')
 def check_url(url_id):
-    url_name = request.args.get('url_name')
-    check_result = check(url_name)
-    if check_result:
-        check_result['url_id'] = url_id
-        url_data.save_check(check_result)
-        flash('Страница успешно проверена', 'success')
-    else:
-        flash('Произошла ошибка при проверке', 'danger')
-    return redirect(url_for('show_url', url_id=url_id))
+    conn = db_access.get_db_conn(app.config['DATABASE_URL'])
+    with conn:
+        url_name = request.args.get('url_name')
+        check_result = check(url_name)
+        if check_result:
+            check_result['url_id'] = url_id
+            db_access.save_check(conn, check_result)
+            flash('Страница успешно проверена', 'success')
+        else:
+            flash('Произошла ошибка при проверке', 'danger')
+        response = redirect(url_for('show_url', url_id=url_id))
+    conn.close()
+    return response
 
 
 @app.errorhandler(404)
--- page_analyzer/db_access.py
+++ page_analyzer/db_access.py
@@ -3,107 +3,87 @@ from psycopg2.extras import NamedTupleCursor
 from datetime import datetime
 
 
-class UrlData:
-    def __init__(self, db_url):
-        self.db_url = db_url
+def get_db_conn(db_url):
+    return psycopg2.connect(db_url)
 
-    def __get_db_conn(self):
-        return psycopg2.connect(self.db_url)
 
-    def get_url_id(self, url_name):
-        sql = 'SELECT id FROM urls WHERE name = %s;'
-        conn = self.__get_db_conn()
-        with conn:
-            with conn.cursor() as cur:
-                cur.execute(sql, (url_name,))
-                entry = cur.fetchone()
-        conn.close()
+def get_url_id(conn, url_name):
+    sql = 'SELECT id FROM urls WHERE name = %s;'
+    with conn.cursor() as cur:
+        cur.execute(sql, (url_name,))
+        entry = cur.fetchone()
         if entry:
             return entry[0]
         return None
 
-    def get_url_info(self):
-        sql = '''
-            SELECT DISTINCT ON (u.created_at)
-                u.id,
-                u.name,
-                c.created_at::date,
-                c.status_code
-            FROM urls u
-            LEFT JOIN url_checks c ON u.id = c.url_id
-            ORDER BY
-                u.created_at DESC,
-                c.created_at DESC;
-        '''
-        conn = self.__get_db_conn()
-        with conn:
-            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-                cur.execute(sql)
-                entries = cur.fetchall()
-        conn.close()
-        return entries
 
-    def find_url(self, url_id):
-        sql = 'SELECT * FROM urls WHERE id = %s;'
-        conn = self.__get_db_conn()
-        with conn:
-            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-                cur.execute(sql, (url_id,))
-                entry = cur.fetchone()
-        conn.close()
-        return entry
+def get_url_info(conn):
+    sql = '''
+        SELECT DISTINCT ON (u.created_at)
+            u.id,
+            u.name,
+            c.created_at::date,
+            c.status_code
+        FROM urls u
+        LEFT JOIN url_checks c ON u.id = c.url_id
+        ORDER BY
+            u.created_at DESC,
+            c.created_at DESC;
+    '''
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute(sql)
+        return cur.fetchall()
 
-    def save_url(self, url_name):
-        sql = '''
-            INSERT INTO urls (name, created_at)
-            VALUES (%s, %s)
-            RETURNING id;
-        '''
-        conn = self.__get_db_conn()
-        with conn:
-            with conn.cursor() as cur:
-                cur.execute(sql, (url_name, datetime.now()))
-                url_id = cur.fetchone()[0]
-        conn.close()
-        return url_id
 
-    def get_checks(self, url_id):
-        sql = '''
-            SELECT * FROM url_checks
-            WHERE url_id = %s
-            ORDER BY created_at DESC;
-        '''
-        conn = self.__get_db_conn()
-        with conn:
-            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-                cur.execute(sql, (url_id,))
-                entries = cur.fetchall()
-        conn.close()
-        return entries
+def find_url(conn, url_id):
+    sql = 'SELECT * FROM urls WHERE id = %s;'
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute(sql, (url_id,))
+        return cur.fetchone()
 
-    def save_check(self, check_result):
-        sql = '''
-            INSERT INTO url_checks (
-                url_id,
-                status_code,
-                h1,
-                title,
-                description,
-                created_at
-            )
-            VALUES (
-                %(url_id)s,
-                %(status_code)s,
-                %(h1)s,
-                %(title)s,
-                %(description)s,
-                %(created_at)s
-            );
-        '''
-        check_result['created_at'] = datetime.now()
-        conn = self.__get_db_conn()
-        with conn:
-            with conn.cursor() as cur:
-                cur.execute(sql, check_result)
-        conn.close()
+
+def save_url(conn, url_name):
+    sql = '''
+        INSERT INTO urls (name, created_at)
+        VALUES (%s, %s)
+        RETURNING id;
+    '''
+    with conn.cursor() as cur:
+        cur.execute(sql, (url_name, datetime.now()))
+        return cur.fetchone()[0]
+
+
+def get_checks(conn, url_id):
+    sql = '''
+        SELECT * FROM url_checks
+        WHERE url_id = %s
+        ORDER BY created_at DESC;
+    '''
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute(sql, (url_id,))
+        return cur.fetchall()
+
+
+def save_check(conn, check_result):
+    sql = '''
+        INSERT INTO url_checks (
+            url_id,
+            status_code,
+            h1,
+            title,
+            description,
+            created_at
+        )
+        VALUES (
+            %(url_id)s,
+            %(status_code)s,
+            %(h1)s,
+            %(title)s,
+            %(description)s,
+            %(created_at)s
+        );
+    '''
+    check_result['created_at'] = datetime.now()
+    with conn.cursor() as cur:
+        cur.execute(sql, check_result)
         return
